ec297e53d8effb567f85cef69106658556072ac3
Jalal Naveed <syednaveedjalal@gmail.com>
2018-10-29 07:57:37 +0200

    parameter parsing in eventshandler

--- hello-world/src/Events.js
+++ hello-world/src/Events.js
@@ -3,12 +3,42 @@ import React, {Component} from 'react';
 
 class MyEvents extends Component {
 
+constructor(props){
+    super(props);
+    this.state = {
+
+        counter:5
+    }
+    // this.incrementCounter = this.incrementCounter.bind(this);
+}
+clickHandler(){
+
+    console.log('Click Pressed')
+ }
+    incrementCounter(param,ev){
+        console.log(ev,'ev');
+        console.log(param,'param');
+    //  this.setState({
+    //     counter: this.state.counter + 100
+    //  })
+    }
+     
+
     render(){
 
         return(
             <div>
-            <h1>Hello Events</h1>
-            <button>This is my First Button in react</button>
+            <h1>My Events</h1>
+            {/* <button onClick={function(){
+                console.log(' FirstButton')
+            }}>FirstButton</button>
+
+<button onClick={this.clickHandler}>Second Button</button> */}
+{/* <button onClick= {this.incrementCounter.bind(this)} >Press Me</button> */}
+<button onClick= {this.incrementCounter.bind(this,'Hello')} >Press Me</button>
+<br></br>
+<span>{this.state.counter}</span>
+<button onClick= {this.incrementCounter.bind(this,'Events')} >Press Me</button> 
 
             </div>
         )
