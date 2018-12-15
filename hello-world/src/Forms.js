5a1ae909d3c6316de4af8920d207784a12edf02f
Jalal Naveed <syednaveedjalal@gmail.com>
2018-12-15 10:56:56 +0200

    checkboxgrouphandler

--- hello-world/src/Forms.js
+++ hello-world/src/Forms.js
@@ -7,15 +7,65 @@ constructor(props){
     this.state = {
 
         username: '',
-        allUsers: []
+        allUsers: [],
+        radioGroup: {
+            MEANStack : false,
+            MERNStack : true,
+            LAMPStack: false
+        },
+        checkboxGroup:{
+            MongoDB:false,
+            Express:false,
+            React:true,
+            Node:false
+
+
+
+
+        }
     }
     this.formhandler= this.formhandler.bind(this);
+    this.clickHandler= this.clickHandler.bind(this);
+    this.radioHandler= this.radioHandler.bind(this);
+    this.checkboxHandler= this.checkboxHandler.bind(this);
+    
 }
 
 formhandler(event){
 // console.log(event.target.value,'event',event);
 this.setState({
     username : event.target.value
+})
+}
+clickHandler(event){
+let currentUser = this.state.allUsers;
+currentUser.push(this.state.username);
+this.setState({
+    allUsers : currentUser,
+    username:''
+    
+})
+console.log(this.state.allUsers);
+}
+radioHandler(event){
+    console.log(event.target.value)
+    let radioGroup = this.state.radioGroup;
+    for (var key in radioGroup){
+        radioGroup[key]= false
+    } 
+    radioGroup[event.target.value] = event.target.checked;
+
+
+    this.setState({
+radioGroup: radioGroup
+    })
+
+}
+checkboxHandler(event){
+let checkbox = this.state.checkboxGroup;
+checkbox[event.target.value] = event.target.checked;
+this.setState({
+    checkboxGroup : checkbox
 })
 
 
@@ -27,8 +77,54 @@ render(){
 
 <h1>My Forms in ReactJS </h1>
 <input type= 'text' name= 'username' value= {this.state.username }onChange={this.formhandler}></input>
-<button> onClick={} </button>
-<p> {this.state.username} </p>
+
+<br/>
+
+Developers Category:
+<br/>
+<label>
+MEAN Stack <input type= 'radio' name='devcategory' value='MEANStack' checked={this.state.radioGroup.MEANStack}onChange={this.radioHandler} />
+</label>
+<br/>
+<label>
+MERN Stack <input type= 'radio' name='devcategory' value='MERNStack' checked={this.state.radioGroup.MERNStack}onChange={this.radioHandler}/>
+</label>
+<br/>
+<label>
+LAMP Stack <input type= 'radio' name='devcategory' value='LAMPStack' checked={this.state.radioGroup.LAMPStack}onChange={this.radioHandler}/>
+</label>
+<br/>
+
+
+
+
+Technologies:
+<br/>
+<label>
+MongoDB <input type= 'checkbox' name='tech' value='MongoDB' checked={this.state.checkboxGroup.MongoDB}onChange={this.checkboxHandler} />
+</label>
+<br/>
+<label>
+Express<input type= 'checkbox' name='tech' value='Express' checked={this.state.checkboxGroup.Express}onChange={this.checkboxHandler}/>
+</label>
+<br/>
+<label>
+React <input type= 'checkbox' name='tech' value='React' checked={this.state.checkboxGroup.React}onChange={this.checkboxHandler}/>
+</label>
+<br/>
+<label>
+Node <input type= 'checkbox' name='tech' value='Node' checked={this.state.checkboxGroup.Node}onChange={this.checkboxHandler}/>
+</label>
+<br/>
+
+
+
+
+
+
+
+<button onClick={this.clickHandler}> Save Me </button>
+{/* <p> {this.state.username} </p> */}
 
 
 
